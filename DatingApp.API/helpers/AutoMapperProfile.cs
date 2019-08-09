b957a67db5fe8f684c744e7e527ffa73e983c344
William Chandler <william.d.chandler1@gmail.com>
2019-08-09 14:36:11 -0400

    added additional fields to the user models and added controlers photos and users.

---
+++ DatingApp.API/helpers/AutoMapperProfile.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.DTOs;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserForListDto>()
+            .ForMember(dest => dest.PhotoUrl, opt =>
+            {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(dest => dest.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+            CreateMap<User, UserForDetailedDto>()
+            .ForMember(dest => dest.PhotoUrl, opt =>
+            {
+                opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+            })
+            .ForMember(dest => dest.Age, opt =>
+            {
+                opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
+            });
+            CreateMap<Photo, PhotosForDetailedDto>();
+        }
+    }
+}
